2e88cafbb1d821104bc0b28f9ad13624bd3a473f
Ralfmat <mateuszwanda@outlook.com>
2025-11-24 21:09:52 +0100

    github actions

--- psiagram/psiagram/settings.py
+++ psiagram/psiagram/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from decouple import config
-from datetime import timedelta
+import os
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -10,13 +11,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-21qmrpvcks7gd*@*60-&bnloo5@xt_t#xklv8@3(za#dhgub*j'
-
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-twoj-lokalny-klucz')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -114,11 +113,17 @@ WSGI_APPLICATION = 'psiagram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"postgres://{os.environ.get('DB_USER')}:{os.environ.get('DB_PASSWORD')}@{os.environ.get('DB_HOST')}:{os.environ.get('DB_PORT')}/{os.environ.get('DB_NAME')}",
+        conn_max_age=600
+    )
 }
 
 
