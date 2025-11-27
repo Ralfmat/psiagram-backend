0e996136f472bce9f4be85f6336420fa7ad80c16
Ralfmat <mateuszwanda@outlook.com>
2025-11-27 11:25:18 +0100

    kurwa

--- psiagram/psiagram/settings.py
+++ psiagram/psiagram/settings.py
@@ -109,9 +109,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'psiagram.wsgi.application'
 
-Database
-https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
