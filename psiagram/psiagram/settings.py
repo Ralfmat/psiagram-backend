7689c690e55ecd570bb153ed651ff7ba821a1275
Ralfmat <mateuszwanda@outlook.com>
2025-11-27 11:39:44 +0100

    STATIC_ROOT = '/tmp/static' added

--- psiagram/psiagram/settings.py
+++ psiagram/psiagram/settings.py
@@ -158,6 +158,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = '/tmp/static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
